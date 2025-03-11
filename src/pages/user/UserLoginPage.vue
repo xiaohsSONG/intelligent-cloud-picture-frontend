98f9548e5b19ce6a335899f50cdd7a1c7e73cda7
xiaohsSONG <3140142060@qq.com>
2025-03-11 22:23:23 +0800

    feat/用户管理

---
+++ src/pages/user/UserLoginPage.vue
@@ -0,0 +1,84 @@
+<template>
+  <div id="userLoginPage">
+    <h2 class="title">智能云图库 - 用户登录</h2>
+    <div class="desc">企业级智能协同云图库</div>
+    <a-form :model="formState" name="basic" autocomplete="off" @finish="handleSubmit">
+      <a-form-item name="userAccount" :rules="[{ required: true, message: '请输入账号' }]">
+        <a-input v-model:value="formState.userAccount" placeholder="请输入账号" />
+      </a-form-item>
+      <a-form-item
+        name="userPassword"
+        :rules="[
+          { required: true, message: '请输入密码' },
+          { min: 8, message: '密码长度至少8位' },
+        ]"
+      >
+        <a-input v-model:value="formState.userPassword" placeholder="请输入密码" />
+      </a-form-item>
+      <div class="tips">
+        没有账号？
+        <RouterLink to="/user/register">立即注册</RouterLink>
+      </div>
+      <a-form-item>
+        <a-button type="primary" html-type="submit" style="width: 100%">登录</a-button>
+      </a-form-item>
+    </a-form>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { userLoginUsingPost } from '@/api/userController'
+import { useLoginUserStore } from '@/stores/user'
+import { message } from 'ant-design-vue'
+import { reactive } from 'vue'
+import { useRouter } from 'vue-router'
+
+const formState = reactive<API.UserLoginRequest>({
+  userAccount: '',
+  userPassword: '',
+})
+
+const router = useRouter()
+const loginUserStore = useLoginUserStore()
+
+//登录提交表单
+const handleSubmit = async (values: API.UserLoginRequest) => {
+  const res = await userLoginUsingPost(values)
+  //登录成功，将状态保存到全局状态中
+  if (res.data.code === 0 && res.data.data) {
+    await loginUserStore.fetchLoginUser()
+    message.success('登录成功')
+    router.push({
+      path: '/',
+      replace: true,
+    })
+  } else {
+    message.error('登录失败，' + res.data.message)
+  }
+}
+</script>
+
+<style scoped>
+#userLoginPage {
+  max-width: 360px;
+  margin: 0 auto;
+}
+
+.title {
+  text-align: center;
+  margin-bottom: 16px;
+}
+
+.desc {
+  text-align: center;
+  color: #bbb;
+  margin-bottom: 16px;
+}
+
+.tips {
+  margin-bottom: 16px;
+  color: #bbb;
+  font-size: 13px;
+  text-align: right;
+}
+</style>
